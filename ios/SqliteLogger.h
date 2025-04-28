7815dca312628b92a24621abecebb69c45c00ed7
bedo <bedo@ezway.eu>
2025-04-28 20:33:41 +0200

    Fix SqliteLogger header, New Arch not supported yet

--- ios/SqliteLogger.h
+++ ios/SqliteLogger.h
@@ -1,12 +1,5 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNSqliteLoggerSpec.h"
-
-@interface SqliteLogger : NSObject <NativeSqliteLoggerSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface SqliteLogger : NSObject <RCTBridgeModule>
-#endif
 
 @end
